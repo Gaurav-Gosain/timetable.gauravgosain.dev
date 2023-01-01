704a7a2d1d5aa21b0dc326bd49afaff809091ba2
Gaurav Gosain <itsgauravgosain@gmail.com>
2023-01-01 20:18:21 +0400

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,37 @@
+import "@/styles/globals.css";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { Analytics } from "@vercel/analytics/react";
+import { Fragment, useState } from "react";
+
+function MyApp({ Component, pageProps }) {
+  const [supabase] = useState(() =>
+    createBrowserSupabaseClient({
+      cookieOptions: {
+        // check if we are in production
+        name: "sb:token",
+        domain:
+          process.env.NODE_ENV === "production" ? "knowfly.org" : "localhost",
+        path: "/",
+        sameSite: "lax",
+        secure: "secure",
+      },
+    })
+  );
+
+  return (
+    <Fragment>
+      <SessionContextProvider
+        supabaseClient={supabase}
+        initialSession={pageProps.initialSession}
+      >
+        <div className="min-h-screen bg-dark font-poppins text-white">
+          <Component {...pageProps} />
+        </div>
+      </SessionContextProvider>
+      <Analytics />
+    </Fragment>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,41 @@
+import timetable from "@/data/timetable.json";
+import { useUser } from "@supabase/auth-helpers-react";
+import Head from "next/head";
+
+export default function Home() {
+  const user = useUser();
+
+  return (
+    <>
+      <Head>
+        <title>Timetable</title>
+        <meta name="description" content="Knowfly Timetable" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/logo_light.png" />
+      </Head>
+      <div className="flex min-h-screen w-screen flex-col items-center justify-center gap-4 py-8 text-center lg:text-4xl">
+        {user ? (
+          <div>Logged into the dashboard! ({user?.email})</div>
+        ) : (
+          <>
+            Not Authorized
+            <span>
+              (login on{" "}
+              <a
+                className="text-blue-500 underline"
+                href="https://www.knowfly.org"
+              >
+                https://www.knowfly.org
+              </a>
+              )
+            </span>
+          </>
+        )}
+
+        <pre className="max-w-screen max-h-[50vh] overflow-scroll text-left text-xs">
+          <code>{JSON.stringify(timetable, null, 2)}</code>
+        </pre>
+      </div>
+    </>
+  );
+}
