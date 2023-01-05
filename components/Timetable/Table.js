e3b415412ee6401bf90d8cd80a3200c15b0730af
Gaurav Gosain <itsgauravgosain@gmail.com>
2023-01-05 15:19:20 +0400

    update table

--- components/Timetable/Table.js
+++ components/Timetable/Table.js
@@ -1,118 +1,75 @@
-import React from 'react'
+import React from "react";
 
-const Table = () => {
+const Table = ({ subjects, setSubjects }) => {
   return (
     <>
-      <div className="mt-12 flex flex-col items-center justify-center space-y-3 overflow-hidden">
+      {/* <div className="mt-12 flex flex-col items-center justify-center space-y-3 overflow-hidden">
         <h1 className="text-3xl font-[500] text-white">My Exam Timetable</h1>
         <button className="rounded-xl bg-black px-2 py-1 text-white">
           Sort by Date
         </button>
-      </div>
+      </div> */}
 
-      <div class="relative mx-32 my-8 overflow-x-auto shadow-md sm:rounded-lg">
-        <table class="w-full text-left text-sm text-gray-500 dark:text-gray-400">
-          <thead class="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
+      <div className="relative mx-32 my-8 max-h-96 max-w-[95vw] overflow-x-auto rounded-2xl shadow-md">
+        <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
+          <thead className="sticky top-0 bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Subject Name
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Type
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Code
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Duration
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Date
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Remove
               </th>
             </tr>
           </thead>
 
           <tbody>
-            <tr class="border-b bg-white dark:border-gray-700 dark:bg-gray-800">
-              <th
-                scope="row"
-                class="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white"
+            {subjects.map((subject) => (
+              <tr
+                className="border-b bg-white dark:border-gray-700 dark:bg-gray-800"
+                key={subject.code}
               >
-                Accounting Paper 1
-              </th>
-              <td class="px-6 py-4">Sliver</td>
-              <td class="px-6 py-4">Laptop</td>
-              <td class="px-6 py-4">$2999</td>
-              <td class="px-6 py-4">$2999</td>
-              <td class="px-6 py-4">
-                <button
-                  className="mx-4 rounded-full bg-red-500 px-2 font-bold text-white transition-all duration-300 hover:bg-red-400"
-                  onClick={() =>
-                    setSelectedSubs(
-                      selectedSubs.filter((arrItem) => arrItem !== currVal)
-                    )
-                  }
+                <th
+                  scope="row"
+                  className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white"
                 >
-                  -
-                </button>
-              </td>
-            </tr>
-            <tr class="border-b bg-white dark:border-gray-700 dark:bg-gray-800">
-              <th
-                scope="row"
-                class="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white"
-              >
-                Accounting Paper 2
-              </th>
-              <td class="px-6 py-4">Sliver</td>
-              <td class="px-6 py-4">Laptop</td>
-              <td class="px-6 py-4">$2999</td>
-              <td class="px-6 py-4">$2999</td>
-              <td class="px-6 py-4">
-                <button
-                  className="mx-4 rounded-full bg-red-500 px-2 font-bold text-white transition-all duration-300 hover:bg-red-400"
-                  onClick={() =>
-                    setSelectedSubs(
-                      selectedSubs.filter((arrItem) => arrItem !== currVal)
-                    )
-                  }
-                >
-                  -
-                </button>
-              </td>
-            </tr>
-            <tr class="bg-white dark:bg-gray-800">
-              <th
-                scope="row"
-                class="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white"
-              >
-                Urdu Paper 1
-              </th>
-              <td class="px-6 py-4">Sliver</td>
-              <td class="px-6 py-4">Laptop</td>
-              <td class="px-6 py-4">$2999</td>
-              <td class="px-6 py-4">$2999</td>
-              <td class="px-6 py-4">
-                <button
-                  className="mx-4 rounded-full bg-red-500 px-2 font-bold text-white transition-all duration-300 hover:bg-red-400"
-                  onClick={() =>
-                    setSelectedSubs(
-                      selectedSubs.filter((arrItem) => arrItem !== currVal)
-                    )
-                  }
-                >
-                  -
-                </button>
-              </td>
-            </tr>
+                  {subject.subject}
+                </th>
+                <td className="px-6 py-4">{subject.type}</td>
+                <td className="px-6 py-4">{subject.code}</td>
+                <td className="px-6 py-4">{subject.duration}</td>
+                <td className="px-6 py-4">{subject.date}</td>
+                <td className="px-6 py-4">
+                  <button
+                    className="mx-4 rounded-full bg-red-500 px-2 font-bold text-white transition-all duration-300 hover:bg-red-400"
+                    onClick={() =>
+                      setSelectedSubs(
+                        selectedSubs.filter((arrItem) => arrItem !== currVal)
+                      )
+                    }
+                  >
+                    -
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
     </>
   );
-}
+};
 
-export default Table
+export default Table;
--- pages/timetable.js
+++ pages/timetable.js
@@ -1,23 +1,57 @@
 import Table from "@/components/Timetable/Table";
+import { ZoneMap } from "@/data/zone_map";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect } from "react";
 
 const TimetablePage = () => {
   const router = useRouter();
   const { code, zone } = router.query;
 
+  const [zoneData, setZoneData] = React.useState(null);
+  const [selectedSubjects, setSelectedSubjects] = React.useState([]);
+  const [groupedSubjects, setGroupedSubjects] = React.useState([]);
+
+  useEffect(() => {
+    setZoneData(ZoneMap[parseInt(zone)]);
+  }, [zone]);
+
+  useEffect(() => {
+    if (zoneData) {
+      let filteredSubjects = [];
+      if (typeof code === "string") {
+        filteredSubjects = zoneData.filter((x) => x.code === code);
+      } else {
+        filteredSubjects = zoneData.filter((x) => code.includes(x.code));
+      }
+
+      setGroupedSubjects(filteredSubjects);
+
+      // flatten the array
+      const subjects = filteredSubjects.map((x) => x.group).flat();
+      setSelectedSubjects(subjects);
+    }
+  }, [zoneData, code]);
+
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center gap-4">
-      <div>Code list:</div>
-      <div>
-        <pre>
-          <code>{JSON.stringify(code, null, 2)}</code>
-        </pre>
+    <div className="mt-12 flex min-h-screen w-screen flex-col items-center justify-center gap-4">
+      {groupedSubjects.map((group) => (
+        <div
+          className="flex flex-col items-center justify-center gap-4"
+          key={group.code}
+        >
+          <h1 className="text-3xl font-bold text-white">
+            {group.commonSubstring}
+          </h1>
+          <Table subjects={group.group} setSubjects={setSelectedSubjects} />
+        </div>
+      ))}
+
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-3xl font-bold text-white">Combined</h1>
+        <Table subjects={selectedSubjects} setSubjects={setSelectedSubjects} />
       </div>
-      <div>Zone = Zone-{zone}</div>
-      <Table />
       <button
-        className="rounded-full bg-primary px-6 py-1 text-dark"
+        className="mb-12 rounded-full bg-primary px-6 py-1 text-dark"
         onClick={() => {
           router.push("/");
         }}
