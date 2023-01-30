bc065a225d1a4f7fe56888dd79b5a7422eb4acc4
Gaurav Gosain <itsgauravgosain@gmail.com>
2023-01-30 16:05:55 +0400

    adsense

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3525004719017767"
+          crossorigin="anonymous"
+        />
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
