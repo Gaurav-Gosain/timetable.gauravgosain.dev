d78fffd48036b41b6a9eb4e8c613899a161e4fc9
Gaurav Gosain <itsgauravgosain@gmail.com>
2023-02-04 19:14:44 +0400

    update loading screen

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,6 @@
 import VerifySession from "@/utils/VerifySession";
 import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
@@ -30,15 +31,22 @@ const Login = () => {
   }, [session]);
 
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center gap-4 px-8 text-center text-xl md:text-4xl">
-      <div>
-        Logging you in... (if you do not get redirected automatically, please
-        try refreshing the page)
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-4 px-8 text-center text-3xl md:text-4xl">
+      <div>Logging you in...</div>
+      <div className="text-sm md:text-base">
+        (if you do not get redirected automatically, please try refreshing the
+        page)
       </div>
       <div>
         {/* loading spinner */}
         <div className="h-32 w-32 animate-spin rounded-full border-b-4 border-white" />
       </div>
+      <Link
+        href="https://www.knowfly.org/tools"
+        className="text-sm text-blue-300 underline"
+      >
+        Go back to Knowfly
+      </Link>
     </div>
   );
 };
